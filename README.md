21002030dec7ecb07c2c37fcb1b31699c4fccf7f
marioEinsis <mario@cluml.com>
2023-12-04 23:58:54 -0800

    Add check and pool options
    Add pool options
    
    Add ci.yml
    
    Remove set from setter functions
    
    Add different init options and update ReadMe
    
    Fix minor grammar error
    
    Install redis in github workflow
    
    Fix workflow file
    
    Remove mac and windows from workflow
    
    Add macOS to the workflow
    
    Fix if statements for workflow file
    
    Remove mac from workflow
    
    Add macOs with cleanup to the workflow
    
    Add macOS to matrix.os
    
    Fix syntax errors in workflow file
    
    Fix minor changes and update README

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # log-broker
+
+A library that supports five logging levels: ERROR, WARNING, INFO, TRACING,
+and DEBUG. The log broker forwards these logs to either a Redis server,
+to a local log file, or both. When forwarded to a a Redis server, it will
+be formatted to a JSON whilst when forwarded to a log file it will be formated
+as a log from the tracing library.
+
+## Usage
+
+```rs
+    // To initialize tracing to a local file when given a valid path
+    // and package name
+    // Note: cannot be `_` otherwise the WokerGuard will immediately
+    // be consumed
+    let _guard = init_tracing(path, pkg_name)?;
+
+    // To initialize connection to Redis server with default settings
+    let _ = init_redis_connection(ip_addr, port, client_id)
+        .await
+        .map_err(|e| e.to_string());
+
+    // To initialize connection to Redis server with custom settings
+    let _ =
+        init_redis_connection_with_options(ip_addr, port, client_id.clone(), Some(pool_options))
+            .await
+            .map_err(|e| e.to_string());
+
+    // Trace log logs to local log file
+    trace!(LogLocation::Local, "{}", trace_log);
+
+    // Debug log logs to Redis server initialized above
+    debug!(LogLocation::Local, "{}", debug_log);
+
+    // Info log logs to both local and Redis server
+    info!(LogLocation::Both, "{}", info_log);
+
+    // Warn and error macros are also available with either options
+    warn!(LogLocation::Both, "{}", warn_log);
+    error!(LogLocation::Both, "{}", error_log);
+
+```
+
+## Test
+
+Run log broker while Redis server is running at 127.0.0.1:6379 with:
+
+```sh
+cargo test --tests
+```
+
+## License
+
+Copyright 2022-2023 ClumL Inc.
+
+Licensed under [Apache License, Version 2.0][apache-license] (the "License");
+you may not use this crate except in compliance with the License.
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See [LICENSE](LICENSE) for
+the specific language governing permissions and limitations under the License.
+
+## Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the [Apache-2.0
+license][apache-license], shall be licensed as above, without any additional
+terms or conditions.
+
+[apache-license]: http://www.apache.org/licenses/LICENSE-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 use anyhow::{anyhow, bail, Ok, Result};
-use bb8_redis::{bb8::Pool, redis::cmd, RedisConnectionManager};
+use bb8_redis::{
+    bb8::{CustomizeConnection, ErrorSink, ManageConnection, NopErrorSink, Pool, QueueStrategy},
+    redis::cmd,
+    RedisConnectionManager,
+};
 use lazy_static::lazy_static;
-use std::{fs::File, net::IpAddr, path::Path, sync::Arc};
+use std::{fs::File, net::IpAddr, path::Path, sync::Arc, time::Duration};
 use tokio::sync::RwLock;
 use tracing::metadata::LevelFilter;
 use tracing_appender::non_blocking::WorkerGuard;
@@ -185,6 +189,114 @@ pub enum LogLocation {
     Both,
 }
 
+pub struct RedisConnPoolOptions<M: ManageConnection> {
+    max_size: u32,
+    min_idle: Option<u32>,
+    test_on_check_out: bool,
+    max_lifetime: Option<Duration>,
+    idle_timeout: Option<Duration>,
+    connection_timeout: Duration,
+    retry_connection: bool,
+    error_sink: Box<dyn ErrorSink<M::Error>>,
+    reaper_rate: Duration,
+    queue_strategy: QueueStrategy,
+    connection_customizer: Option<Box<dyn CustomizeConnection<M::Connection, M::Error>>>,
+}
+
+impl<M: ManageConnection> Default for RedisConnPoolOptions<M> {
+    fn default() -> Self {
+        Self {
+            max_size: 10,
+            min_idle: None,
+            test_on_check_out: false,
+            max_lifetime: Some(Duration::from_secs(30 * 60)),
+            idle_timeout: Some(Duration::from_secs(10 * 60)),
+            connection_timeout: Duration::from_secs(30),
+            retry_connection: true,
+            error_sink: Box::new(NopErrorSink),
+            reaper_rate: Duration::from_secs(30),
+            queue_strategy: QueueStrategy::default(),
+            connection_customizer: None,
+        }
+    }
+}
+
+impl<M: ManageConnection> RedisConnPoolOptions<M> {
+    #[must_use]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    #[must_use]
+    pub fn max_size(mut self, max_size: u32) -> Self {
+        self.max_size = max_size;
+        self
+    }
+
+    #[must_use]
+    pub fn min_idle(mut self, min_idle: Option<u32>) -> Self {
+        self.min_idle = min_idle;
+        self
+    }
+
+    #[must_use]
+    pub fn test_on_check_out(mut self, test_on_check_out: bool) -> Self {
+        self.test_on_check_out = test_on_check_out;
+        self
+    }
+
+    #[must_use]
+    pub fn max_lifetime(mut self, max_lifetime: Option<Duration>) -> Self {
+        self.max_lifetime = max_lifetime;
+        self
+    }
+
+    #[must_use]
+    pub fn max_lifetimeidle_timeout(mut self, idle_timeout: Option<Duration>) -> Self {
+        self.idle_timeout = idle_timeout;
+        self
+    }
+
+    #[must_use]
+    pub fn connection_timeout(mut self, connection_timeout: Duration) -> Self {
+        self.connection_timeout = connection_timeout;
+        self
+    }
+
+    #[must_use]
+    pub fn retry_connection(mut self, retry_connection: bool) -> Self {
+        self.retry_connection = retry_connection;
+        self
+    }
+
+    #[must_use]
+    pub fn error_sink(mut self, error_sink: Box<dyn ErrorSink<M::Error>>) -> Self {
+        self.error_sink = error_sink;
+        self
+    }
+
+    #[must_use]
+    pub fn reaper_rate(mut self, reaper_rate: Duration) -> Self {
+        self.reaper_rate = reaper_rate;
+        self
+    }
+
+    #[must_use]
+    pub fn queue_strategy(mut self, queue_strategy: QueueStrategy) -> Self {
+        self.queue_strategy = queue_strategy;
+        self
+    }
+
+    #[must_use]
+    pub fn connection_customizer(
+        mut self,
+        connection_customizer: Box<dyn CustomizeConnection<M::Connection, M::Error>>,
+    ) -> Self {
+        self.connection_customizer = Some(connection_customizer);
+        self
+    }
+}
+
 lazy_static! {
     static ref CLIENT_ID: Arc<RwLock<String>> = Arc::new(RwLock::new(String::new()));
     static ref REDIS_POOL: Arc<RwLock<Option<Pool<RedisConnectionManager>>>> =
@@ -193,8 +305,9 @@ lazy_static! {
 
 /// Init redis connection
 ///
-/// This functions takes in the ip address, port and client id
-/// and stores it for later use in a connection pool
+/// This functions takes in the ip address, port, and client id
+/// and stores it for later use in a connection pool with default
+/// Redis connection pool settings
 ///
 /// # Errors
 ///
@@ -207,23 +320,91 @@ pub async fn init_redis_connection(
     client_id: String,
 ) -> anyhow::Result<()> {
     let manager = RedisConnectionManager::new(format!("redis://{ip_addr}:{port}"))?;
-    let pool = Pool::builder().build(manager).await?;
+    let pool = build_pool_with_options::<RedisConnectionManager>(manager, None).await?;
 
-    {
-        let mut pool_lock = REDIS_POOL
-            .try_write()
-            .map_err(|e| anyhow!("Redis pool error: {}", e))?;
-        *pool_lock = Some(pool);
+    if pool.get().await.is_ok() {
+        {
+            let mut pool_lock = REDIS_POOL
+                .try_write()
+                .map_err(|e| anyhow!("Redis pool error: {}", e))?;
+            *pool_lock = Some(pool.clone());
+        }
+
+        {
+            let mut client_id_lock = CLIENT_ID
+                .try_write()
+                .map_err(|e| anyhow!("Redis client id error: {}", e))?;
+            *client_id_lock = client_id;
+        }
+        Ok(())
+    } else {
+        bail!("Redis connection error")
+    }
+}
+
+/// Init redis connection with custom pool options
+///
+/// This functions takes in the ip address, port, client id, and pool options
+/// and stores it for later use in a connection pool with custom Redis
+/// connection pool settings
+///
+/// # Errors
+///
+/// * Redis connection error
+/// * Redis pool error
+/// * Redis client id error
+pub async fn init_redis_connection_with_options(
+    ip_addr: IpAddr,
+    port: u16,
+    client_id: String,
+    pool_options: Option<RedisConnPoolOptions<RedisConnectionManager>>,
+) -> anyhow::Result<()> {
+    let manager = RedisConnectionManager::new(format!("redis://{ip_addr}:{port}"))?;
+    let pool = build_pool_with_options::<RedisConnectionManager>(manager, pool_options).await?;
+
+    if pool.get().await.is_ok() {
+        {
+            let mut pool_lock = REDIS_POOL
+                .try_write()
+                .map_err(|e| anyhow!("Redis pool error: {}", e))?;
+            *pool_lock = Some(pool.clone());
+        }
+
+        {
+            let mut client_id_lock = CLIENT_ID
+                .try_write()
+                .map_err(|e| anyhow!("Redis client id error: {}", e))?;
+            *client_id_lock = client_id;
+        }
+        Ok(())
+    } else {
+        bail!("Redis connection error")
+    }
+}
+
+#[allow(dead_code)]
+async fn build_pool_with_options<M: ManageConnection>(
+    manager: M,
+    pool_options: Option<RedisConnPoolOptions<M>>,
+) -> Result<Pool<M>, <M as ManageConnection>::Error> {
+    let pool_options = pool_options.unwrap_or_default();
+    let mut builder = Pool::builder()
+        .max_size(pool_options.max_size)
+        .min_idle(pool_options.min_idle)
+        .test_on_check_out(pool_options.test_on_check_out)
+        .max_lifetime(pool_options.max_lifetime)
+        .idle_timeout(pool_options.idle_timeout)
+        .connection_timeout(pool_options.connection_timeout)
+        .retry_connection(pool_options.retry_connection)
+        .error_sink(pool_options.error_sink)
+        .reaper_rate(pool_options.reaper_rate)
+        .queue_strategy(pool_options.queue_strategy);
+
+    if let Some(connection_customizer) = pool_options.connection_customizer {
+        builder = builder.connection_customizer(connection_customizer);
     }
 
-    {
-        let mut client_id_lock = CLIENT_ID
-            .try_write()
-            .map_err(|e| anyhow!("Redis client id error: {}", e))?;
-        *client_id_lock = client_id;
-    }
-
-    Ok(())
+    builder.build(manager).await
 }
 
 #[allow(dead_code)]
